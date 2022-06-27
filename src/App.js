e756703e91694243aba65154fd7d5accb63a2c69
Binsar <bfmcoding@gmail.com>
2022-06-28 00:19:51 +0700

    pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [shortEffect, setShortEffect] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
+  const [flavor, setFlavor] = useState([]);
+  const url = 'https://pokeapi.co/api/v2/ability';
+
+  const handleClick = async () => {
+    try {
+      const data = await fetch(`${url}/${search}`);
+      const res = await data.json();
+      setShortEffect(res.effect_entries);
+      setPokemon(res.pokemon);
+      setFlavor(res.flavor_text_entries);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Pokemon Ability</h1>
+      <input
+        type='text'
+        placeholder='input ability...'
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={handleClick}>Search</button>
+      <p>
+        <b>Short Effect: </b>
+        {shortEffect.map((data, i) => {
+          return <span key={i}>{data.short_effect}</span>;
+        })}
+      </p>
+      <p>
+        <b>Pokemon: </b>
+        {pokemon.map((data, i) => {
+          return <span key={i}>{data.pokemon.name},</span>;
+        })}
+      </p>
+      <p>
+        <b>Flavor: </b>
+        {flavor.map((data, i) => {
+          if (data.language.name === 'en') {
+            return <span key={i}>{data.flavor_text},</span>;
+          }
+          return null;
+        })}
+      </p>
     </div>
   );
 }
